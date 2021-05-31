9ba3c4fab3a38c8fc89a41a316362ba28856bb93
Ayman <akh.lora@gmail.com>
2021-05-31 13:20:07 +0200

    firstcommit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV ==='production') {
     app.use(express.static('bakverkproject/build'))
 }
 
-//app.use(express.static('bakverkproject/build'))
+app.use(express.static('bakverkproject/build'))
 
 /*app.get('*', (req, res) => {
     res.sendFile(path.join('bakverkproject/build/index.html'))

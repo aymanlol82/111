d6fe0fb895948a6a4bdc610c213d84faf0258a4b
Ayman <akh.lora@gmail.com>
2021-05-31 13:24:03 +0200

    firstcommit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import Configurations from './src/configurations/Configurations.js'
 import routes from './src/routes/route.js'
 import cors from 'cors'
 import path from 'path'
-import mongoose from 'mongoose'
 
 
 const app = express()
@@ -16,39 +15,26 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan('common',))
 
-mongoose.connect(
-    process.env.MONOGODB_URI || "mongodb://localhost:27017/backendproject",
-    {useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true}, () => {
-        console.log('   ✔️     Successfuly connected to the databas.. ')
-    }
-)
 
 routes.pastryRoute(app)
 routes.userRoute(app)
 
-const PORT = process.env.PORT || 8080
 
 if (process.env.NODE_ENV ==='production') {
     app.use(express.static('bakverkproject/build'))
 }
 
-app.use(express.static('bakverkproject/build'))
+//app.use(express.static('bakverkproject/build'))
 
 /*app.get('*', (req, res) => {
     res.sendFile(path.join('bakverkproject/build/index.html'))
 })*/
 
-app.listen (PORT , () => {
-    console.log(`   ✔️   Server running on port : ${PORT} `)
-})    
-
-
-
 
 //app.use(Middlewares.notFound)
 
-//Configurations.connectToPort(app)
-//Configurations.connectToDatabas()
+Configurations.connectToPort(app)
+Configurations.connectToDatabas()
 
 export default app
 

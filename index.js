098e4bad3678beaf4361f4fe7ed2d8ef29e70634
Ayman <akh.lora@gmail.com>
2021-05-31 13:13:00 +0200

    firstcommit

--- index.js
+++ index.js
@@ -6,39 +6,49 @@ import Configurations from './src/configurations/Configurations.js'
 import routes from './src/routes/route.js'
 import cors from 'cors'
 import path from 'path'
+import mongoose from 'mongoose'
 
 
 const app = express()
-const __dirname = path.resolve()
+app.use(cors())
+app.use(express.json())
+
+app.use(helmet())
+app.use(morgan('common',))
+
+mongoose.connect(
+    process.env.MONOGODB_URI || "mongodb://localhost:27017/backendproject",
+    {useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true}, () => {
+        console.log('   ✔️     Successfuly connected to the databas.. ')
+    }
+)
+
+routes.pastryRoute(app)
+routes.userRoute(app)
+
+const PORT = process.env.PORT || 8080
 
 if (process.env.NODE_ENV ==='production') {
     app.use(express.static('bakverkproject/build'))
 }
 
-
-
-/*app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname ,'bakverkproject/build/index.html'))
-})*/
+//app.use(express.static('bakverkproject/build'))
 
 /*app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'bakverkproject/build', 'index.html'));
+    res.sendFile(path.join('bakverkproject/build/index.html'))
 })*/
 
-
-app.use(express.json())
-app.use(cors({Credential: true}))
-app.use(helmet())
-app.use(morgan('common',))
+app.listen (PORT , () => {
+    console.log(`   ✔️   Server running on port : ${PORT} `)
+})    
 
 
-routes.pastryRoute(app)
-routes.userRoute(app)
 
-app.use(Middlewares.notFound)
 
-Configurations.connectToPort(app)
-Configurations.connectToDatabas()
+//app.use(Middlewares.notFound)
+
+//Configurations.connectToPort(app)
+//Configurations.connectToDatabas()
 
 export default app
 
